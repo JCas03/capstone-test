d684cf2d46e6e6d1dbcbd55d4ae0f972681c0ba2
congnguyenhcl <96488290+congnguyenhcl@users.noreply.github.com>
2022-02-21 16:38:28 -0600

    Convert Azure DevOps repo into Github

--- src/main/java/com/hcl/ppmtool/PpmtoolApplication.java
+++ src/main/java/com/hcl/ppmtool/PpmtoolApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class PpmtoolApplication {
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
