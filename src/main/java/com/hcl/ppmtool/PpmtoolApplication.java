8928d2f783dc1777ad6727073bcb5f485ef4a83d
James-L-Jr <james.f.logan2@gmail.com>
2022-02-22 11:34:21 -0600

    log4j additions

--- src/main/java/com/hcl/ppmtool/PpmtoolApplication.java
+++ src/main/java/com/hcl/ppmtool/PpmtoolApplication.java
@@ -1,5 +1,7 @@
 package com.hcl.ppmtool;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -17,13 +19,16 @@ import java.util.Date;
 @SpringBootApplication
 
 public class PpmtoolApplication {
+	static Logger log = Logger.getLogger(PpmtoolApplication.class.getName());
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    
     public static void main(String[] args) {
+    	PropertyConfigurator.configure("src/main/resources/log4j.properties");
+		log.warn("DB accessed authorized from Application..");
         SpringApplication.run(PpmtoolApplication.class, args);
     }
-
 
 }
